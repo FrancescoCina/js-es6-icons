8d077ff2671177ebd04ffe223a2ba7d7ef9fdc28
FrancescoCina <francesco.cina04@gmail.com>
2021-07-26 16:24:26 +0200

    Stampate le icone in HTML, gestito offset e sistemata grafica.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 // STAMPARE CON CICLO FOR EACH SU ARRAY ICONS LE CARTE IN HTML
+
+let iconElement = document.getElementById("icon");
+
+let totalIcons = "";
+let printIcon = icons.forEach((icon, index) => {
+
+    // Gestione OFFSET
+    let hasOffset = "";
+    if (index % 5 === 0) {
+        hasOffset = "offset-md-1";
+    }
+    icon =
+        `
+    <div class="col col-sm-4 col-md-2 ${hasOffset}">
+        <div class="card">
+            <div class="card-body">
+                <i class="${icon.family} ${icon.prefix}${icon.name} fa-3x ${icon.type}"></i>
+                <h6 class="py-2 text-center">${icon.name}</h6>
+            </div>
+        </div>
+    </div>
+    `
+    totalIcons += icon;
+    iconElement.innerHTML = totalIcons;
+});
+
+
+
+
